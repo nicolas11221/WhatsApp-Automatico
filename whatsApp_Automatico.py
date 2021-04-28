687728d42b77c7fe80b34a87ae0cbd4ffe65d614
Jean Carlos A <jaldea@tejesoft.com>
2021-04-28 15:36:44 -0400

    Change multiples messages for a List of messages Se cambia las multiples variables de mensajes, por una lista que se itrerara

--- whatsApp_Automatico.py
+++ whatsApp_Automatico.py
@@ -3,21 +3,17 @@ import pyautogui as pg
 import time as tm
 import webbrowser as web
 
-whatsapp_number = 1111111
-message1 = "Example Message"
-message2 = "Example Message"
-message3 = "Example Message"
+index = 0
+whatsapp_number = 56953955540
+messages = ["Message", "Message", "Message", "Message"]
 
 web.open(f"https://web.whatsapp.com/send?phone=+{whatsapp_number}")
 tm.sleep(8) #Delay for messages
-pg.write(f"{message} 1") # Your message
-pg.press('enter') #Send
 
-for i in range(1):
-    pg.write(f"{message1} 1 \n")
-    pg.write(f"{message2} 2 \n")
-    pg.write(f"{message3} 3")
-    pg.press('enter')
-    
-    print("Mensajes " +str(i+1)) #Bucle for
+for message in messages:
+    if message:
+        pg.write(f"{message} \n")
+        pg.press('enter')
+        index += 1
+        print(f"{message} {index}")
 pass
