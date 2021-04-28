49dafb0155e7866b5446953e615f51cd37cd0015
Nicolas Garcia <nicolas.garcia11221@gmail.com>
2021-04-28 15:00:39 -0400

    Commit With tree message

--- whatsApp_Automatico.py
+++ whatsApp_Automatico.py
@@ -4,7 +4,9 @@ import time as tm
 import webbrowser as web
 
 whatsapp_number = 1111111
-message = "Example Message"
+message1 = "Example Message"
+message2 = "Example Message"
+message3 = "Example Message"
 
 web.open(f"https://web.whatsapp.com/send?phone=+{whatsapp_number}")
 tm.sleep(8) #Delay for messages
@@ -12,9 +14,9 @@ pg.write(f"{message} 1") # Your message
 pg.press('enter') #Send
 
 for i in range(1):
-    pg.write(f"{message} 1 \n")
-    pg.write(f"{message} 2 \n")
-    pg.write(f"{message} 3")
+    pg.write(f"{message1} 1 \n")
+    pg.write(f"{message2} 2 \n")
+    pg.write(f"{message3} 3")
     pg.press('enter')
     
     print("Mensajes " +str(i+1)) #Bucle for
